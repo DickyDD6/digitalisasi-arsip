a72aae7c1338d3ed0340543ff424c565aec24fa0
aldiipratama <paldi0013@gmail.com>
2026-01-16 17:23:15 +0700

    feat: Implement user management features including user editing, password generation, and user statistics
    - Added EditUserModal component for editing user details.
    - Introduced FieldGeneratePassword component for password generation and management.
    - Created UsersStatsSummary component to display user statistics.
    - Developed UsersTableCard component for displaying user data in a table format with edit and delete options.
    - Updated ManageUserPage to integrate new components for user management.
    - Added necessary dependencies for password generation and radio group functionality.
    - Enhanced input group components for better usability and styling.

--- apps/web/app/dashboard/manage-user/page.tsx
+++ apps/web/app/dashboard/manage-user/page.tsx
@@ -1,5 +1,34 @@
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardTitle,
+} from "@repo/ui/components/card";
+import { Metadata } from "next";
+import { UsersStatsSummary } from "./_components/widgets/users-stats-summary";
+import { UsersTableCard } from "./_components/widgets/users-table-card";
+
+export const metadata: Metadata = {
+	title: "User Management",
+	description: "Manage user accounts and their roles in the system",
+};
+
 const ManageUserPage = () => {
-	return <div>ManageUserPage</div>;
+	return (
+		<>
+			<Card>
+				<CardContent className="space-y-1">
+					<CardTitle>User Management</CardTitle>
+					<CardDescription>
+						Manage user accounts and their roles in the system
+					</CardDescription>
+				</CardContent>
+			</Card>
+
+			<UsersStatsSummary />
+			<UsersTableCard />
+		</>
+	);
 };
 
 export default ManageUserPage;
