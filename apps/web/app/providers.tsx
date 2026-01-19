07030de96c1c993c1bbf5f93a22b25c9e889f0eb
aldiipratama <paldi0013@gmail.com>
2026-01-19 14:50:37 +0700

    chore: remove unused AuthBootstrap and AuthGuard components

--- apps/web/app/providers.tsx
+++ apps/web/app/providers.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import { AuthBootstrap } from "@/features/auth/components/auth-bootstrap";
-import { AuthGuard } from "@/features/auth/components/auth-guard";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { PropsWithChildren } from "react";
@@ -11,8 +9,6 @@ export const TanstackQueryProvider = ({ children }: PropsWithChildren) => {
 
 	return (
 		<QueryClientProvider client={queryClient}>
-			<AuthBootstrap />
-			<AuthGuard />
 			{children}
 			<ReactQueryDevtools initialIsOpen={false} />
 		</QueryClientProvider>
